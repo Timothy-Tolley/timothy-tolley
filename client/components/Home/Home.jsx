e97ccd7ece3d2b9cd5ccd92f6c0970c48053e68f
timothy-tolley <timothytolley@outlook.com>
2018-04-01 23:50:42 +1200

    added placeholder ready to go

--- client/components/Home/Home.jsx
+++ client/components/Home/Home.jsx
@@ -2,17 +2,19 @@ import React from 'react'
 import {connect} from 'react-redux'
 
 import './home.css'
-import Banner from '../Banner/Banner'
-import Contact from '../Contact/Contact'
-import Projects from '../Projects/Projects'
+// import Banner from '../Banner/Banner'
+// import Contact from '../Contact/Contact'
+// import Projects from '../Projects/Projects'
+import PlaceHolder from '../PlaceHolder/PlaceHolder'
 
 class Home extends React.Component {
   render () {
     return (
       <div className = 'home-container'>
-        <Banner />
+        <PlaceHolder />
+        {/* <Banner />
         <Projects />
-        <Contact />
+        <Contact /> */}
       </div>
     )
   }
---
+++ client/components/PlaceHolder/PlaceHolder.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import {connect} from 'react-redux'
+
+import './placeholder.css'
+
+class PlaceHolder extends React.Component {
+  render () {
+    return (
+      <div className = 'placeholder-container'>
+        <div className = 'placeholder-content'>
+          <h1 className = 'placeholder-header'> Timothy Tolley Freelance</h1>
+          <h2 className = 'placeholder-header2'> Web Development Services for Small Businesses </h2>
+          <p className = 'placeholder-text'> Thanks for Stopping By!</p>
+          <p className = 'placeholder-text'> My full website is coming soon but in the meantime, feel free to get in touch below! </p>
+          <div className = 'placeholder-link-cont'>
+            <a href="mailto:timothytolley@outlook.com" className = 'placeholder-link'>
+              <img src = '/images/mail.png' alt = 'mail logo' className = 'ext-logo'/>
+            </a>
+            <a href="https://github.com/Timothy-Tolley" className = 'placeholder-link'>
+              <img src = '/images/github.png' className = 'ext-logo' alt = 'github logo'/>
+            </a>
+            <a href="https://www.linkedin.com/in/timothy-tolley/" className = 'placeholder-link'>
+              <img src = '/images/linkedin.png' className = 'ext-logo' alt = 'github logo'/>
+            </a>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default connect()(PlaceHolder)
